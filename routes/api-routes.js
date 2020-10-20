44e5623b69798663140d44d56cd51beb3598b528
James Lamb <jameslamb13@gmail.com>
2020-10-20 18:32:25 -0400

    html routes are established. server.js is created along with the port being connected.

---
+++ routes/api-routes.js
@@ -0,0 +1,22 @@
+const path = require("path")
+var db = require("../models");
+
+
+module.exports = function(app){
+// Route to get last workout
+// probably want to use index of after a sort
+  
+    app.get("/api/workouts", function(req,res) {
+
+  });
+
+  // Route to add an exercise
+  // /api/workouts/:id
+  
+  // Route to create workout
+  // /api/workouts
+
+ // to get workouts in range..?
+ // /api/workouts/range
+
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Setting up PORTS
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Requiring Routes
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+
+
+// Connections
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}!`)
+})
